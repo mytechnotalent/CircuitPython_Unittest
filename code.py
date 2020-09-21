430ead3a79b98673bf0147d613553bcfbc3c0d55
Kevin Thomas <kevin@mytechnotalent.com>
2020-09-21 12:29:33 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,16 @@
+def foo(param):
+    """Simple foo function that returns a string with an exclamation point
+    
+    Parameters
+    ----------
+    param : str
+      Param string to pass in
+    
+    Returns
+    -------
+    str
+      A str with a passed in param concat with an exclamation point
+    """
+    param = param + '!'
+    return param
+    
